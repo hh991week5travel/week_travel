cfa6ce67da93b15c121ecae84eb58d52b8f5f794
daegyu-jeong <fade8667@naver.com>
2022-06-10 17:42:53 +0900

    travel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import Post from "./pages/Post";
+import PostDetail from "./pages/PostDetail";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/postDetail/:postId" element={<PostDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
