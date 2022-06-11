f5332de33b24e68973270724be0c3fa20a8f43ef
daegyu-jeong <fade8667@naver.com>
2022-06-11 23:11:45 +0900

    day1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+const callAxios = async () => {
+  await axios.get("http://localhost:5001/travel")
+};
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  React.useEffect(() => {
+    callAxios();
+  }, []);
 
-export default Login
+  return (
+    <div>
+      <p>
+        이메일 : <input />{" "}
+      </p>
+      <p>
+        비밀번호 : <input />{" "}
+      </p>
+      <button> 로그인 </button>
+      <br />
+      <Link to="/SignUp">
+        <button> 회원가입 </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Login;
