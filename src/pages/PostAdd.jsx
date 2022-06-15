d970f016767732891fb724ac18d794de2742c7c9
daegyu-jeong <fade8667@naver.com>
2022-06-15 16:23:45 +0900

    5일차 저녁

--- src/pages/PostAdd.jsx
+++ src/pages/PostAdd.jsx
@@ -1,54 +1,60 @@
 //게시물 작성 페이지
 
-import React, { useRef } from 'react'
-import Header from '../components/Header'
-import Image from '../components/ImageUpload'
-import Input from '../element/Input'
-import Content from '../element/Content'
-import Button from '../element/Button'
-import './PostAdd.css'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { __addPost } from '../redux/modules/post'
-import { getCookie } from '../shared/Cookie'
-
+import React, { useRef } from "react";
+import Header from "../components/Header";
+import Image from "../components/ImageUpload";
+import Input from "../element/Input";
+import Content from "../element/Content";
+import Button from "../element/Button";
+import "./PostAdd.css";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { __addPost } from "../redux/modules/post";
+import { getCookie } from "../shared/Cookie";
 
 const Post = () => {
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const titleRef = useRef(null)
-  const contentRef = useRef(null)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const titleRef = useRef(null);
+  const contentRef = useRef(null);
 
   const addPost = (e) => {
-    dispatch(__addPost({
-      title : titleRef.current.value,
-      content : contentRef.current.value,
-      // imgUrl : imgUrl.current.value,
-      token : getCookie("Authorization")
-    }))
-    navigate('/Main')
-  }
+    dispatch(
+      __addPost({
+        title: titleRef.current.value,
+        content: contentRef.current.value,
+        // imgUrl : imgUrl.current.value,
+        token: getCookie("Authorization"),
+      })
+    );
+    navigate("/Main");
+  };
 
   return (
     <>
-    <Header />
-      <div className='container'>
-      <Image />
-      <span>제목</span>
-      <Input ref={titleRef}/>
-      <span>내용</span>
-      <Content ref={contentRef}/>
-      </div>
+      <Header />
+      <HomeBody>
+        <div className="container">
+          <Image />
+          <span>제목</span>
+          <Input ref={titleRef} />
+          <span>내용</span>
+          <Content ref={contentRef} />
+        </div>
 
-      <div className='footer'>
+        <div className="footer">
+          <Button onClick={addPost}>작성 완료</Button>
+        </div>
+      </HomeBody>
+    </>
+  );
+};
 
-        <Button onClick={addPost}>작성 완료</Button>
-
-      </div>
-
-  </>
-  )
-}
+const HomeBody = styled.div`
+  height: 100vh;
+  background: url(../../background.jpg) center center no-repeat;
+  background-size: cover;
+`;
 
 export default Post;
